e34d71d804aaf3843677bd5bf2a5b10b6238c07c
kemalgencay <kemalgencay@gmail.com>
2022-03-04 18:48:38 +0300

    iki başlıklı

--- src/components/header.js
+++ src/components/header.js
@@ -9,54 +9,54 @@ const Header = () => {
       <div className={styles.header}>
         <StaticImage
           alt="bir Istanbul manzarası"
-          src="../../content/images/header1.jpg"
+          src="../../content/images/ogrenmek.jpg"
         ></StaticImage>
-        <div>
-          <h1 className={styles.title}>Öğrenmek yaparak olur</h1>
-        </div>
-        <nav>
-          <ul className={styles.navList}>
-            <li>
-              <Link
-                className={styles.navItem}
-                activeClassName={styles.activeNavItem}
-                Link
-                to="/"
-              >
-                Ana Sayfa
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={styles.navItem}
-                activeClassName={styles.activeNavItem}
-                to="/about"
-                ti
-              >
-                Hakkimda
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={styles.navItem}
-                activeClassName={styles.activeNavItem}
-                to="/blog"
-              >
-                Dersler
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={styles.navItem}
-                activeClassName={styles.activeNavItem}
-                to="/yazi"
-              >
-                Görüşlerim
-              </Link>
-            </li>
-          </ul>
-        </nav>
       </div>
+      <div>
+        <h1 className={styles.title}>Öğrenmek yaparak olur</h1>
+      </div>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              Link
+              to="/"
+            >
+              Ana Sayfa
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              to="/about"
+              ti
+            >
+              Hakkimda
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              to="/blog"
+            >
+              Dersler
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              to="/yazi"
+            >
+              Görüşlerim
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
