e3142a5edbf16e39fc158c5b5b442fd53f1e53b1
kemalgencay <kemalgencay@gmail.com>
2022-04-03 06:39:18 +0300

    Deno post

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
 import React from "react"
 const Footer = () => {
   return (
-    <span>
+    <p>
+      {" "}
       © {new Date().getFullYear()} Kemal Gençay,{" "}
       <a href="https://www.gatsbyjs.org"> Gatsby</a> ile yapılmıştır.{" "}
-    </span>
+      <image src="../"></image>
+    </p>
   )
 }
 
